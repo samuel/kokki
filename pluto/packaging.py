4d49d4c4305336c8b060230c9525a3bded262804
Samuel Stauffer <samuel@descolada.com>
2009-11-22 14:50:12 -0800

    Made the default action for Package resource "install"

--- pluto/packaging.py
+++ pluto/packaging.py
@@ -6,6 +6,7 @@ from pluto.base import *
 from pluto.providers.package.apt import DebianAPTProvider
 
 class Package(Resource):
+    action = ResourceArgument(default="install")
     package_name = ResourceArgument()
     version = ResourceArgument()
     actions = ["install", "upgrade", "remove", "purge"]
