6d26e7008285cf6a6c82488be0c32cf56f77ee16
Samuel Stauffer <samuel@descolada.com>
2009-11-28 17:39:33 -0800

    Made the cookbook importer more robust. Now behaves like normal imports.

--- pluto/cookbook.py
+++ pluto/cookbook.py
@@ -42,52 +42,80 @@ class CookbookTemplate(object):
     def setup(self):
         pass
 
-COOKBOOKS_MOD = "pluto.cookbooks"
+COOKBOOKS_NAMESPACE = "pluto.cookbooks"
 
 class CookbookImporter(object):
     def __init__(self):
-        self.cookbooks_module = imp.new_module("cookbooks")
+        mod = self.cookbooks_module = imp.new_module("cookbooks")
+        mod.__path__ = list(cookbook_paths)
+        mod.__file__ = "<%s>" % self.__class__.__name__
+        sys.modules[COOKBOOKS_NAMESPACE] = mod
+        pluto.cookbooks = mod
 
     def find_module(self, fullname, path=None):
-        if not fullname.startswith('pluto.cookbooks'):
+        if not fullname.startswith(COOKBOOKS_NAMESPACE):
             return None
 
-        if fullname == COOKBOOKS_MOD:
+        if self._find_module(fullname):
             return self
 
-        name = fullname[len(COOKBOOKS_MOD)+1:]
+    def _find_module(self, fullname, path=None):
+        mod_path = fullname[len(COOKBOOKS_NAMESPACE)+1:]
+        current_name = "%s" % COOKBOOKS_NAMESPACE
+        paths = path or list(cookbook_paths)
+        for name in mod_path.split('.'):
+            current_name += "."+name
+            if current_name in sys.modules:
+                paths = sys.modules[current_name].__path__
+                continue
 
-        return self
+            return imp.find_module(name, paths)
+
+    def _load_module(self, fullname, fp, pathname, description):
+        if description[2] == 5:
+            filename = os.path.join(pathname, "__init__.py")
+            ispkg = True
+        else:
+            filename = pathname
+            ispkg = False
+
+        mod = imp.new_module(fullname)
+        mod.__file__ = filename
+        if ispkg:
+            mod.__path__ = [pathname]
+        sys.modules[fullname] = mod
+
+        try:
+            if fp:
+                exec compile(fp.read(), filename, "exec") in mod.__dict__
+            else:
+                execfile(filename, mod.__dict__)
+        except:
+            del sys.modules[fullname]
+            raise
+
+        mod.__loader__ = self
+        mod.__package__ = COOKBOOKS_NAMESPACE
+
+        return mod
 
     def load_module(self, fullname):
         if fullname in sys.modules:
             return sys.modules[fullname]
 
-        if fullname == COOKBOOKS_MOD:
+        if fullname == COOKBOOKS_NAMESPACE:
             mod = self.cookbooks_module
-            mod.__path__ = [fullname]
+            mod.__path__ = list(cookbook_paths)
             mod.__file__ = "<%s>" % self.__class__.__name__
             sys.modules[fullname] = mod
         else:
-            mod = None
-            cb_name = fullname.split('.')[-1]
-            for cp in cookbook_paths:
-                cb_path = os.path.join(cp, cb_name, "__init__.py")
-                if os.path.exists(cb_path):
-                    mod = imp.new_module(fullname)
-                    mod.__file__ = cb_path
-                    mod.__path__ = ["%s/%s" % (COOKBOOKS_MOD.replace('.', '/'), cb_name)]
-                    sys.modules[fullname] = mod
-                    try:
-                        execfile(cb_path, mod.__dict__)
-                    except:
-                        del sys.modules[fullname]
-                        raise
-            if not mod:
-                return None
+            fp, pathname, description = self._find_module(fullname)
+            mod = self._load_module(fullname, fp, pathname, description)
 
-        mod.__loader__ = self
-        mod.__package__ = "cookbooks"
+            name = fullname[len(COOKBOOKS_NAMESPACE)+1:]
+            if "." not in name:
+                setattr(self.cookbooks_module, name, mod)
+
         return mod
 
 def load_cookbook(name, path=None, env=None):
@@ -103,7 +131,8 @@ def load_cookbook(name, path=None, env=None):
         for path in paths:
             cb_path = os.path.join(path, name)
             if os.path.exists(os.path.join(cb_path, 'metadata.yaml')):
-                mod = __import__("pluto.cookbooks.%s" % name, {}, {}, [name])
+                # mod = __import__("%s.%s" % (COOKBOOKS_NAMESPACE, name), {}, {}, [])
+                mod = __import__(COOKBOOKS_NAMESPACE, {}, {}, [name])
                 template = CookbookTemplate(name, cb_path)
                 for k in dir(template):
                     if not hasattr(mod, k):
