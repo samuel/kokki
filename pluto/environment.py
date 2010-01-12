c3847fb52ad03e55411faf0a34ed18d0c78d4310
Samuel Stauffer <samuel@descolada.com>
2010-01-12 15:42:05 -0800

    Made AttributeDictionary a bit more robust. Now dictionary values are converted to AttributeDictionary is necessary

--- pluto/environment.py
+++ pluto/environment.py
@@ -76,13 +76,25 @@ class System(object):
         return out.strip().split("\n")
 
 class AttributeDictionary(dict):
-    def __setattr__(self, key, value):
-        self[key] = value
+    def __setattr__(self, name, value):
+        self[name] = value
 
-    def __getattr__(self, key):
-        if key in self:
-            return self[key]
-        raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__.__name__, key))
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__.__name__, name))
+
+    def __setitem__(self, name, value):
+        super(AttributeDictionary, self).__setitem__(name, self._convert_value(value))
+
+    def __getitem__(self, name):
+        return self._convert_value(super(AttributeDictionary, self).__getitem__(name))
+
+    def _convert_value(self, value):
+        if isinstance(value, dict) and not isinstance(value, AttributeDictionary):
+            return AttributeDictionary(value)
+        return value
 
 class Environment(AttributeDictionary):
     system = System()
