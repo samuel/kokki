072d5b9747b27d899569677cf9cb535b52e36be9
Samuel Stauffer <samuel@descolada.com>
2010-01-14 18:42:44 -0800

    Moved all resources to be under resources/

--- kokki/__init__.py
+++ kokki/__init__.py
@@ -5,10 +5,8 @@ from kokki import cookbook
 from kokki.base import *
 from kokki.cookbook import *
 from kokki.environment import *
-from kokki.packaging import *
 from kokki.providers import *
 from kokki.recipe import *
+from kokki.resources import *
 from kokki.runner import *
-from kokki.service import *
 from kokki.source import *
-from kokki.system import *
---
+++ kokki/resources/__init__.py
@@ -0,0 +1,3 @@
+from kokki.resources.packaging import *
+from kokki.resources.service import *
+from kokki.resources.system import *
