de4001bd462eeb6b5c2e6ece5cea91e90a7a23c2
Samuel Stauffer <samuel@descolada.com>
2010-03-16 17:02:40 -0700

    Fixed a typeo trying to import accounts resources

--- kokki/resources/__init__.py
+++ kokki/resources/__init__.py
@@ -1,4 +1,4 @@
-from kokki.resources.account import *
+from kokki.resources.accounts import *
 from kokki.resources.packaging import *
 from kokki.resources.service import *
 from kokki.resources.system import *
