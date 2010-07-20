0ec12fbd49e197edfa88b3d2d19d80a3237d0029
Gavin McQuillan <gavin@gmac.local>
2010-07-20 11:53:54 -0700

    modified:   providers/service/debian.py

--- kokki/providers/service/debian.py
+++ kokki/providers/service/debian.py
@@ -2,6 +2,7 @@ import subprocess
 
 from kokki.base import Fail
 from kokki.providers import Provider
+from kokki.providers.service import ServiceProvider
 
 class DebianServiceProvider(ServiceProvider):
     def enable_runlevel(self,runlevel):
