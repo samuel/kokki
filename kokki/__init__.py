cd86098a513443b7fa47ad13f46b939ff1e44c69
Samuel Stauffer <samuel@descolada.com>
2010-09-07 17:17:12 -0700

    Removed unused imports

--- kokki/__init__.py
+++ kokki/__init__.py
@@ -1,12 +1,10 @@
-# from kokki import cookbook
+
 from kokki.base import *
-# from kokki.cookbook import *
 from kokki.environment import *
 from kokki.exceptions import *
 from kokki.providers import *
 from kokki.recipe import *
 from kokki.resources import *
-# from kokki.runner import *
 from kokki.source import *
 from kokki.version import *
 
