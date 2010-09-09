a7d18b3df6978a205cdedef782e4777db154adf6
Samuel Stauffer <samuel@descolada.com>
2010-09-08 18:47:56 -0700

    Renamed the metadata files

---
+++ kokki/cookbooks/memcached/metadata.py
@@ -0,0 +1,24 @@
+
+__description__ = "Memcached shared memory cache daemon"
+__config__ = {
+    "memcached.memory": dict(
+        description = "Memory allocated for memcached instance in megabytes",
+        default = 64,
+    ),
+    "memcached.port": dict(
+        description = "Port to use for memcached instance",
+        default = 11211,
+    ),
+    "memcached.user": dict(
+        description = "User as which to run the memcached instance",
+        default = "nobody",
+    ),
+    "memcached.threads": dict(
+        description = "Number of threads to use to process incoming requests",
+        default = 1,
+    ),
+    "memcached.verbose": dict(
+        description = "Verbose logging output",
+        default = False,
+    ),
+}
