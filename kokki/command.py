422464775c419f239c76a4b6b62eeb2466461665
Samuel Stauffer <samuel@descolada.com>
2010-09-09 15:56:21 -0700

    The command now accepts a directory for -f in which case every python file is compiled and run

--- kokki/command.py
+++ kokki/command.py
@@ -19,15 +19,22 @@ def main():
     if not args:
         parser.error("must specify at least one command")
 
-    path = os.path.dirname(os.path.abspath(options.filename))
+    path = os.path.abspath(options.filename)
+    if not os.path.isdir(path):
+        path = os.path.dirname(path)
     if path not in sys.path:
         sys.path.insert(0, path)
 
-    with open(options.filename, "rb") as fp:
-        kitchen = fp.read()
+    if os.path.isdir(options.filename):
+        files = [os.path.join(path, f) for f in sorted(os.listdir(path)) if f.endswith('.py')]
+    else:
+        files = [options.filename]
 
     globs = {}
-    exec compile(kitchen, options.filename, 'exec') in globs
+    for fname in files:
+        with open(fname, "rb") as fp:
+            source = fp.read()
+        exec compile(source, fname, 'exec') in globs
 
     kit = Kitchen()
     for c in args:
