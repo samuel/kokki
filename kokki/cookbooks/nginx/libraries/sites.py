85e96698b89cba040806e745faa099b3e4458df2
Samuel Stauffer <samuel@descolada.com>
2010-09-09 11:07:05 -0700

    Another fix to 'site' in nginx

--- kokki/cookbooks/nginx/libraries/sites.py
+++ kokki/cookbooks/nginx/libraries/sites.py
@@ -10,7 +10,11 @@ def site(name, enable=True):
     else:
         cmd = 'nxdissite'
 
+    def _not_if():
+        e = exists("%s/sites-enabled/%s" % (env.config.nginx.dir, name))
+        return e if enable else not e
+
     Execute("%s %s" % (cmd, name),
             command = "/usr/sbin/%s %s" % (cmd, name),
             notifies = [("reload", env.resources["Service"]["nginx"])],
-            not_if = lambda:exists("%s/sites-enabled/%s" % (env.config.nginx.dir, name)))
+            not_if = _not_if)
