c420c7004fe18caa554de3b5929c2caf22629368
Samuel Stauffer <samuel@descolada.com>
2010-09-10 16:16:47 -0700

    Added metadata to users cookbook

---
+++ kokki/cookbooks/users/metadata.py
@@ -0,0 +1,8 @@
+
+__description__ = "Manage user accounts and sysadmins"
+__config__ = {
+    "sysadmins": dict(
+        description = "List of sysadmins (id,username,sshkey_id,sshkey_type,sshkey)"
+        default = [],
+    )
+}
