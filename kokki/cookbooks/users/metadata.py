b32269c48f2c6dc8cc6b449933270c8bbd969265
Samuel Stauffer <samuel@descolada.com>
2010-09-10 16:17:06 -0700

    Forgot a comma

--- kokki/cookbooks/users/metadata.py
+++ kokki/cookbooks/users/metadata.py
@@ -2,7 +2,7 @@
 __description__ = "Manage user accounts and sysadmins"
 __config__ = {
     "sysadmins": dict(
-        description = "List of sysadmins (id,username,sshkey_id,sshkey_type,sshkey)"
+        description = "List of sysadmins (id,username,sshkey_id,sshkey_type,sshkey)",
         default = [],
     )
 }
