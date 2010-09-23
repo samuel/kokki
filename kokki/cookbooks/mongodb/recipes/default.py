6d83f2977dc2e2480e8e39558a6f2284dabbf8e6
Samuel Stauffer <samuel@descolada.com>
2010-09-23 15:19:13 -0700

    Make sure to notify upstart immediate about init changes

--- kokki/cookbooks/mongodb/recipes/default.py
+++ kokki/cookbooks/mongodb/recipes/default.py
@@ -46,7 +46,7 @@ File("/etc/init/mongodb.conf",
     group = "root",
     mode = 0644,
     content = Template("mongodb/upstart.conf.j2", variables=dict(mongodb=env.config.mongodb)),
-    notifies = [("run", env.resources["Execute"]["initctl reload mongodb"])])
+    notifies = [("run", env.resources["Execute"]["initctl reload mongodb"], True)])
 
 Service("mongodb")
 File(env.config.mongodb.configpath,
