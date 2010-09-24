e8e055c999c01f3af1128862c2223e834c26698e
Samuel Stauffer <samuel@descolada.com>
2010-09-24 16:12:07 -0700

    Added support booleans to flume services

--- kokki/cookbooks/flume/recipes/master.py
+++ kokki/cookbooks/flume/recipes/master.py
@@ -5,6 +5,9 @@ env.include_recipe("flume")
 
 Package("flume-master")
 Service("flume-master",
+    supports_restart = True,
+    supports_reload = False,
+    supports_status = False,
     subscribes = [
         ("restart", env.resources["File"]["flume-config"]),
         ("restart", env.resources["File"]["flume-site-config"]),
