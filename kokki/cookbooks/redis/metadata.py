69c6fb7e4b509273beccce1967576c604fb7aa35
Samuel Stauffer <samuel@descolada.com>
2010-09-27 11:53:18 -0700

    Added a new redis conf template to include options added in redis 2.0+

--- kokki/cookbooks/redis/metadata.py
+++ kokki/cookbooks/redis/metadata.py
@@ -5,6 +5,10 @@ __config__ = {
         description = "Path to the config file",
         default = "/etc/redis.conf",
     ),
+    "redis.bind": dict(
+        description = "Interface to listen on",
+        default = "127.0.0.1",
+    ),
     "redis.port": dict(
         description = "Accept connections on the specified port",
         default = 6379,
@@ -20,6 +24,14 @@ __config__ = {
     "redis.dbdir": dict(
         description = "For default save/load DB in/from the working directory",
         default = "/var/db/redis/",
+    ),
+    "redis.appendonly": dict(
+        description = "Use the append only file for persistence",
+        default = True,
+    ),
+    "redis.appendfsync": dict(
+        description = "How often to fsync the AOF file (no, everysec, always)"
+        default = "everysec",
     ),
     "redis.databases": dict(
         description = "Set the number of databases.",
@@ -44,13 +56,5 @@ __config__ = {
     "redis.maxmemory": dict(
         description = "Don't use more memory than the specified amount of bytes.",
         default = None,
-    ),
-    "redis.shareobjects": dict(
-        description = "If object sharing should be used.",
-        default = False,
-    ),
-    "redis.shareobjectspoolsize": dict(
-        description = "The size of the object pool.",
-        default = 1024,
     ),
 }
