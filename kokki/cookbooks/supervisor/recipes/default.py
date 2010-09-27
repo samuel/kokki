605cbfd2a65391be0d391632638be674a42b8a88
Samuel Stauffer <samuel@descolada.com>
2010-09-27 08:59:47 -0700

    Updated supervisor recipe

--- kokki/cookbooks/supervisor/recipes/default.py
+++ kokki/cookbooks/supervisor/recipes/default.py
@@ -1,20 +1,20 @@
 
 from kokki import *
 
-include_recipe("monit")
+env.include_recipe("monit")
 
 Package("supervisor",
     provider = "kokki.providers.package.easy_install.EasyInstallProvider")
 
 File("supervisord.conf",
-    path = "%s/supervisord.conf" % env.supervisor.config_path,
+    path = "%s/supervisord.conf" % env.config.supervisor.config_path,
     content = Template("supervisor/supervisord.conf.j2"))
 
 Directory("supervisor.d",
-    path = "%s/supervisor.d" % env.supervisor.config_path)
+    path = "%s/supervisor.d" % env.config.supervisor.config_path)
 
-cookbook.monit.monitrc("supervisord",
+env.cookbooks.monit.rc("supervisord",
     content = Template("supervisor/monit.conf.j2"))
 
-cookbooks.monit.MonitService("supervisord",
+env.cookbooks.monit.MonitService("supervisord",
     subscribes = [("restart", env.resources["File"]["supervisord.conf"])])
