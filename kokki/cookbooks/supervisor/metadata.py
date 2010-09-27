1c355ad9a91fc0e4b0179eee3e1b641e2dc24947
Samuel Stauffer <samuel@descolada.com>
2010-09-27 08:59:10 -0700

    Fix to supervisor cookbook

--- kokki/cookbooks/supervisor/metadata.py
+++ kokki/cookbooks/supervisor/metadata.py
@@ -1,22 +1,22 @@
 
 __description__ = "Process monitoring"
 __config__ = {
-    "supervisor.config_path" = dict(
+    "supervisor.config_path": dict(
         display_name = "Supervisor config path",
         description = "Config file path for supervisor",
         default = "/etc",
     ),
-    "supervisor.binary_path" = dict(
+    "supervisor.binary_path": dict(
         display_name = "Supervisor binary path",
         description = "Path to the supervisor binaries",
         default = "/usr/local/bin",
     ),
-    "supervisor.pidfile" = dict(
+    "supervisor.pidfile": dict(
         display_name = "Supervisor pid file path",
         description = "Path to the supervisor pid file",
         default = "/var/run/supervisord.pid",
     ),
-    "supervisor.logfile" = dict(
+    "supervisor.logfile": dict(
         display_name = "Supervisor log file path",
         description = "Path to the supervisor log file",
         default = "/var/log/supervisord.log",
