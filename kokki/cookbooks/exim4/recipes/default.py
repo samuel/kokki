c01fa74afdce7c2d1b9719a34b491a1b72882d6a
Samuel Stauffer <samuel@descolada.com>
2010-09-28 15:56:07 -0700

    Added authenticated smarthost support to exim4

--- kokki/cookbooks/exim4/recipes/default.py
+++ kokki/cookbooks/exim4/recipes/default.py
@@ -11,3 +11,15 @@ File("/etc/exim4/update-exim4.conf.conf",
     mode = 0644,
     content = Template("exim4/update-exim4.conf.conf.j2"),
     notifies = [("restart", env.resources["Service"]["exim4"])])
+
+File("/etc/exim4/exim4.conf.localmacros",
+    owner = "root",
+    group = "root",
+    mode = 0644,
+    content = "AUTH_CLIENT_ALLOW_NOTLS_PASSWORDS = 1\n",
+    notifies = [("restart", env.resources["Service"]["exim4"])])
+
+File("/etc/exim4/passwd.client",
+    mode = 0600,
+    content = Template("exim4/passwd.client.j2"),
+    notifies = [("restart", env.resources["Service"]["exim4"])])
