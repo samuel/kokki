1091ee95acc7c12e6caffd0dd55bef1d171e268f
Samuel Stauffer <samuel@descolada.com>
2010-10-06 12:13:00 -0700

    Added apache config for nagios3

--- kokki/cookbooks/nagios3/recipes/default.py
+++ kokki/cookbooks/nagios3/recipes/default.py
@@ -2,3 +2,13 @@
 from kokki import *
 
 Package("nagios3")
+Package("apache2")
+
+File("/etc/apache2/sites-available/nagios3",
+    owner = "www-data",
+    group = "www-data",
+    mode = 0644,
+    content = Template("nagios3/apache2-site.j2"),
+    notifies = [("restart", env.resources["Service"]["apache2"])])
+
+env.cookbooks.apache2.site("nagios3")
