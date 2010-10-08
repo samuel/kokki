460460cb95906a71f66a969ab0f2e9d5285b4d21
Samuel Stauffer <samuel@descolada.com>
2010-10-08 15:09:04 -0700

    Update boto on the server

--- kokki/cookbooks/aws/recipes/default.py
+++ kokki/cookbooks/aws/recipes/default.py
@@ -3,10 +3,10 @@ import os
 from kokki import *
 
 # Package("python-boto")
-Execute("pip install git+http://github.com/boto/boto.git#egg=boto",
-    not_if = 'python -c "import boto"')
 Execute("mv /usr/lib/pymodules/python2.6/boto /tmp/boto.orig",
     only_if = lambda:os.path.exists("/usr/lib/pymodules/python2.6/boto"))
+Execute("pip install git+http://github.com/boto/boto.git#egg=boto",
+    not_if = 'python -c "import boto"')
 
 # Mount volumes and format is necessary
 
