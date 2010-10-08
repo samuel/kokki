8dbd2af4610e463e8906763077ebf278853e06a2
Samuel Stauffer <samuel@descolada.com>
2010-10-08 16:13:59 -0700

    Fix busket

--- kokki/cookbooks/busket/recipes/default.py
+++ kokki/cookbooks/busket/recipes/default.py
@@ -3,15 +3,6 @@ from kokki import *
 
 Package("erlang")
 
-import os
-filename = url.rsplit('/', 1)[-1]
-dirname = filename
-while dirname.rsplit('.', 1)[-1] in ('gz', 'tar', 'tgz', 'bz2'):
-    dirname = dirname.rsplit('.', 1)[0]
-
-if not dirname:
-    raise Fail("Unable to figure out directory name of project for URL %s" % url)
-
 Script("install-busket",
     not_if = lambda:os.path.exists(env.config.busket.path),
     cwd = "/usr/local/src",
