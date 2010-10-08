063b697c97c9bd285bcdcfe85f978a61378ac5fb
Samuel Stauffer <samuel@descolada.com>
2010-10-08 16:15:50 -0700

    Fix busket

--- kokki/cookbooks/busket/recipes/default.py
+++ kokki/cookbooks/busket/recipes/default.py
@@ -10,7 +10,7 @@ Script("install-busket",
     code = (
         "git clone git://github.com/samuel/busket.git busket\n"
         "cd busket\n"
-        "make rel\n"
+        "make release\n"
         "mv rel/busket {install_path}\n"
     ).format(install_path=env.config.busket.path)
 )
