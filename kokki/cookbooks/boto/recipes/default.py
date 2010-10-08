efec8dcb7fe413a14df057da1ad3259405887f57
Samuel Stauffer <samuel@descolada.com>
2010-10-08 15:17:07 -0700

    Fixed boto recipe

--- kokki/cookbooks/boto/recipes/default.py
+++ kokki/cookbooks/boto/recipes/default.py
@@ -2,6 +2,8 @@
 from kokki import *
 
 # Package("python-boto")
+Package("pip",
+    provider = "kokki.providers.package.easy_install.EasyInstallProvider")
 Execute("mv /usr/lib/pymodules/python2.6/boto /tmp/boto.orig",
     only_if = lambda:os.path.exists("/usr/lib/pymodules/python2.6/boto"))
 Execute("pip install git+http://github.com/boto/boto.git#egg=boto",
