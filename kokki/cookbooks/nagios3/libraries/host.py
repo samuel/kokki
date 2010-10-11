f23f90422964a5c67da28bc2e744dcee7f00e72d
Samuel Stauffer <samuel@descolada.com>
2010-10-11 14:38:04 -0700

    nagios3: trying to fix deleting of hosts

--- kokki/cookbooks/nagios3/libraries/host.py
+++ kokki/cookbooks/nagios3/libraries/host.py
@@ -16,19 +16,22 @@ def Host(name,
     kwargs['services'] = {}
     kwargs['groups'] = groups
 
+    path = "/etc/nagios3/conf.d/host_%s.cfg" % name.lower()
+
     if action == "delete":
         host = env.config.nagios3.hosts.pop(name, None)
         if host:
             for g in host.get('groups', []):
                 env.config.nagios3.hostgroups[g]['members'].append(name)
+            env.resources["File"][path].action = "delete"
     else:
         env.config.nagios3.hosts[name] = kwargs
         for g in groups:
             env.config.nagios3.hostgroups[g]['members'].append(name)
 
-    kwargs['name'] = name
+        kwargs['name'] = name
 
-    File("/etc/nagios3/conf.d/host_%s.cfg" % name.lower(),
-        content = Template("nagios3/host.cfg.j2", dict(host=kwargs)),
-        action = action,
-        notifies = [("restart", env.resources["Service"]["nagios3"])])
+        File(path,
+            content = Template("nagios3/host.cfg.j2", dict(host=kwargs)),
+            action = action,
+            notifies = [("restart", env.resources["Service"]["nagios3"])])
