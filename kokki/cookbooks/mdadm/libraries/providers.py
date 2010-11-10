fb4799fd7945cc98189144e5846fa9a8221cb4d1
Samuel Stauffer <samuel@descolada.com>
2010-11-10 11:15:12 -0800

    No need to wait for the array as it should be synchronous

--- kokki/cookbooks/mdadm/libraries/providers.py
+++ kokki/cookbooks/mdadm/libraries/providers.py
@@ -15,14 +15,12 @@ class ArrayProvider(Provider):
                     "--metadata", self.resource.metadata,
                     "--raid-devices", str(len(self.resource.devices)),
                 ] + self.resource.devices)
-            self.wait_for_array(True)
             self.resource.updated()
     
     def action_stop(self):
         if self.exists():
             subprocess.check_call(["/sbin/mdadm",
                     "--stop", self.resource.name])
-            self.wait_for_array(False)
             self.resource.updated()
 
     def action_assemble(self):
@@ -30,15 +28,8 @@ class ArrayProvider(Provider):
             subprocess.check_call(["/sbin/mdadm",
                     "--assemble", self.resource.name,
                 ] + self.resource.devices)
-            self.wait_for_array(True)
             self.resource.updated()
 
     def exists(self):
         ret = subprocess.call(["/sbin/mdadm", "-Q", self.resource.name])
         return not ret
-
-    def wait_for_array(self, exists):
-        for i in range(10):
-            if exists == self.exists():
-                break
-            time.sleep(0.5)
