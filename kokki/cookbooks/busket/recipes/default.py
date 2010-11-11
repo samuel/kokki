60755ede64f044d0d61305612c4e44236486c24e
Samuel Stauffer <samuel@descolada.com>
2010-11-10 17:26:16 -0800

    Added service for busket

--- kokki/cookbooks/busket/recipes/default.py
+++ kokki/cookbooks/busket/recipes/default.py
@@ -7,8 +7,17 @@ Package("erlang")
 File("/usr/lib/erlang/man",
     action = "delete")
 
-Package("mercurial",
-    provider = "kokki.providers.package.easy_install.EasyInstallProvider")
+# Package("mercurial",
+#     provider = "kokki.providers.package.easy_install.EasyInstallProvider")
+
+command = os.path.join(env.config.busket.path, "bin", "busket")
+
+Service("busket",
+    start_command = "%s start" % command,
+    stop_command = "%s stop" % command,
+    restart_command = "%s restart" % command,
+    status_command = "%s ping" % command,
+    action = "nothing")
 
 Script("install-busket",
     not_if = lambda:os.path.exists(env.config.busket.path),
@@ -18,5 +27,6 @@ Script("install-busket",
         "cd busket\n"
         "make release\n"
         "mv rel/busket {install_path}\n"
-    ).format(install_path=env.config.busket.path)
+    ).format(install_path=env.config.busket.path),
+    notifies = [("restart", env.resources["Service"]["busket"])]
 )
