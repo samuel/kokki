66ed7a95dff156ff8083ea9f0651a8b1d436c25e
Samuel Stauffer <samuel@descolada.com>
2010-11-11 16:19:11 -0800

    Make sure HOME is set when installing busket. Erlang requires it

--- kokki/cookbooks/busket/recipes/default.py
+++ kokki/cookbooks/busket/recipes/default.py
@@ -25,6 +25,8 @@ Script("install-busket",
     code = (
         "git clone git://github.com/samuel/busket.git busket\n"
         "cd busket\n"
+        "mkdir /tmp/erlhome\n"
+        "export HOME=/tmp/erlhome\n"
         "make release\n"
         "mv rel/busket {install_path}\n"
     ).format(install_path=env.config.busket.path),
