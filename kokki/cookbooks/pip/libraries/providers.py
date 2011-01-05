223cfcdd7f940679bf086e085cd1793bd4444e47
Samuel Stauffer <samuel@descolada.com>
2011-01-05 11:33:51 -0800

    Use easy_install to figure out candidate versions for pip provider as pip doesn't do it

--- kokki/cookbooks/pip/libraries/providers.py
+++ kokki/cookbooks/pip/libraries/providers.py
@@ -1,6 +1,7 @@
 
 __all__ = ["PipPackageProvider"]
 
+import re
 from subprocess import check_call, Popen, PIPE, STDOUT
 from kokki import *
 from kokki.providers.package import PackageProvider
@@ -21,11 +22,31 @@ class PipPackageProvider(PackageProvider):
 
     @property
     def candidate_version(self):
-        return self.resource.version
+        if not hasattr(self, '_candidate_version'):
+            if re.match("^[A-Za-z0-9_.-]+$", self.resource.package_name):
+                p = Popen([self.easy_install_binary_path, "-n", self.resource.package_name], stdout=PIPE, stderr=STDOUT)
+                out = p.communicate()[0]
+                res = p.wait()
+                if res != 0:
+                    self.log.warning("easy_install check returned a non-zero result (%d) %s" % (res, self.resource))
+                #     self._candidate_version = None
+                # else:
+                m = best_match_re.search(out)
+                if not m:
+                    self._candidate_version = None
+                else:
+                    self._candidate_version = m.group(2)
+            else:
+                self._candidate_vesion = self.resource.version
+        return self._candidate_version
 
     @property
     def pip_binary_path(self):
         return "pip"
+
+    @property
+    def easy_install_binary_path(self):
+        return "easy_install"
 
     def install_package(self, name, version):
         check_call([self.pip_binary_path, "install", name], stdout=PIPE, stderr=STDOUT)
