49a924a00c3742479118ffa563d56e669ea660cb
Samuel Stauffer <samuel@descolada.com>
2011-02-03 14:42:13 -0800

    Install erlang before installing RabbitMQ

--- kokki/cookbooks/rabbitmq/recipes/default.py
+++ kokki/cookbooks/rabbitmq/recipes/default.py
@@ -1,6 +1,8 @@
 
 from kokki import *
 
+Package("erlang")
+
 if env.system.platform in ("ubuntu", "debian"):
     pkg_url = "http://www.rabbitmq.com/releases/rabbitmq-server/v2.3.1/rabbitmq-server_2.3.1-1_all.deb"
     Execute("cd /tmp ; wget %s ; dpkg -i %s ; rm rabbitmq*deb" % (pkg_url, pkg_url.rsplit('/', 1)[-1]),
