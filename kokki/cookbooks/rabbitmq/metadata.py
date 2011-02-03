0e8ed5fd245aa1157f78aa385a8625d98bfe3040
Samuel Stauffer <samuel@descolada.com>
2011-02-03 13:50:47 -0800

    Added a preliminary rabbitmq cookbookg

---
+++ kokki/cookbooks/rabbitmq/metadata.py
@@ -0,0 +1,8 @@
+
+__description__ = "RabbitMQ Messaging Server"
+__config__ = {
+    "rabbitmq.path": dict(
+        description = "Install path for rabbitmq",
+        default = "/usr/local/rabbitmq",
+    )
+}
