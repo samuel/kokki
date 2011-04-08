9d3b7b2ee3992f12d0766c6ddd1ee43fc2985c07
Samuel Stauffer <samuel@descolada.com>
2011-04-08 15:40:44 -0700

    Switch to using the APT repository for RabbitMQ

--- kokki/cookbooks/rabbitmq/recipes/default.py
+++ kokki/cookbooks/rabbitmq/recipes/default.py
@@ -1,10 +1,28 @@
 
-from kokki import Package, Execute
+from kokki import Package, Execute, File, Fail
 
 Package("erlang")
 
+apt_list_path = '/etc/apt/sources.list.d/rabbitmq.list'
+apt = None
 if env.system.platform in ("ubuntu", "debian"):
-    pkg_url = "http://www.rabbitmq.com/releases/rabbitmq-server/v2.3.1/rabbitmq-server_2.3.1-1_all.deb"
-    Execute("cd /tmp ; wget %s ; dpkg -i %s ; rm rabbitmq*deb" % (pkg_url, pkg_url.rsplit('/', 1)[-1]),
-        not_if = "dpkg-query -c rabbitmq-server > /dev/null")
+    apt = "deb http://www.rabbitmq.com/debian/ testing main"
 
+if not apt:
+    raise Fail("Can't find a rabbitmq package for your platform/version")
+
+Execute("apt-update-rabbitmq",
+    command = "apt-get update",
+    action = "nothing")
+
+Execute("curl http://www.rabbitmq.com/rabbitmq-signing-key-public.asc | apt-key add -",
+    not_if = "(apt-key list | grep rabbitmq > /dev/null)")
+
+File(apt_list_path,
+    owner = "root",
+    group ="root",
+    mode = 0644,
+    content = apt+"\n",
+    notifies = [("run", env.resources["Execute"]["apt-update-rabbitmq"], True)])
+
+Package("rabbitmq-server")
