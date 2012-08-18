a3dd9e715bb589074178ccfd6f20f378ae9b85fe
Samuel Stauffer <samuel@descolada.com>
2012-08-18 08:35:22 -0700

    postgresql9: no longer need separate repo

--- kokki/cookbooks/postgresql9/recipes/default.py
+++ kokki/cookbooks/postgresql9/recipes/default.py
@@ -1,16 +1,16 @@
 import os
 from kokki import Execute, Package
 
-if not (env.system.platform == "ubuntu" and env.system.lsb['release'] in ["11.10"]):
-    apt_list_path = '/etc/apt/sources.list.d/pitti-postgresql-lucid.list'
+# if not (env.system.platform == "ubuntu" and env.system.lsb['release'] in ["11.10"]):
+#     apt_list_path = '/etc/apt/sources.list.d/pitti-postgresql-lucid.list'
 
-    Execute("apt-update-postgresql9",
-        command = "apt-get update",
-        action = "nothing")
+#     Execute("apt-update-postgresql9",
+#         command = "apt-get update",
+#         action = "nothing")
 
-    apt = None
-    if env.system.platform == "ubuntu":
-        Package("python-software-properties")
-        Execute("add-apt-repository ppa:pitti/postgresql",
-            not_if = lambda:os.path.exists(apt_list_path),
-            notifies = [("run", env.resources["Execute"]["apt-update-postgresql9"], True)])
+#     apt = None
+#     if env.system.platform == "ubuntu":
+#         Package("python-software-properties")
+#         Execute("add-apt-repository ppa:pitti/postgresql -y",
+#             not_if = lambda:os.path.exists(apt_list_path),
+#             notifies = [("run", env.resources["Execute"]["apt-update-postgresql9"], True)])
